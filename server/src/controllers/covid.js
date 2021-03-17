e6a6b3d9a079359395d5f614676eb3a91b567f46
xJosee <3006762980101@ingenieria.usac.edu.gt>
2021-03-16 20:21:35 -0600

    log

--- server/src/controllers/covid.js
+++ server/src/controllers/covid.js
@@ -3,6 +3,7 @@ const covidModel = require('../database/covid.schema');
 module.exports = {
 
     async createRecord(req, res) {
+        console.log(req.body);
         const data = new covidModel(req.body);
         await data.save();
         res.status(200).send({ message: 'caso creado correctamente' });
