b683db09ae5a5bfba6f5904bffce2dd19271a41e
xJosee <3006762980101@ingenieria.usac.edu.gt>
2021-03-16 20:03:39 -0600

    cambiando ruta covid

--- server/src/routes/covid.js
+++ server/src/routes/covid.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/covid')
 
-router.post('/', (req, res) => {
+router.post('/postData', (req, res) => {
     controller.createRecord(req, res);
 });
 
