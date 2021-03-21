6bfe05bcc5f81b76366818c1de10c1dfe17b2d56
Jose Moran <3005377270101@ingenieria.usac,edu.gt>
2021-03-20 21:23:24 -0600

    [ADD]Tabs de graficas y grafica de pastel

--- web-app/src/components/covid-data/charts/bar.js
+++ web-app/src/components/covid-data/charts/bar.js
@@ -1,62 +1,68 @@
 import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
-import { Form } from 'react-bootstrap'
 
 
 function BarGraph() {
     const [data, setData] = useState({
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: [''],
         datasets: [
             {
-                label: 'My First dataset',
+                label: 'Rango de edades',
                 backgroundColor: 'rgba(240, 84, 84, 0.7)',
                 borderColor: 'rgba(48, 71, 94)',
                 borderWidth: 1,
                 hoverBackgroundColor: 'rgba(255,99,132,0.4)',
                 hoverBorderColor: 'rgba(255,99,132,1)',
-                data: [65, 59, 80, 81, 56, 55, 40],
+                data: [0],
 
             }
-
         ],
-
     })
 
 
-    /*useEffect(() => {
+    useEffect(() => {
         const interval = setInterval(() => {
-
             fetchValues()
-
         }, 1500);
         return () => clearInterval(interval);
     }, []);
 
     const fetchValues = () => {
-        fetch('http://34.67.69.50:7000/modulos/getRam')
+        fetch('http://34.67.69.50:7000/agerange')
             .then((response) => response.json())
             .then((json) => {
-                // console.log(parseInt(json.Total, 10) - parseInt(json.Libre, 10), json.Libre)
+                console.log(json)
+                let labels = []
+                for (let i in json) {
+                    if (json[i][0]) {
+                        labels.push(i)
+                    }
+                }
+                let count = []
+                for (let i in json) {
+                    if (json[i][0]) {
+                        count.push(json[i][0].count)
+                    }
+                }
                 const newData = {
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    labels: labels,
                     datasets: [
                         {
-                            label: 'My First dataset',
+                            label: 'Rango de edades',
                             backgroundColor: 'rgba(240, 84, 84, 0.7)',
                             borderColor: 'rgba(48, 71, 94)',
                             borderWidth: 1,
                             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
                             hoverBorderColor: 'rgba(255,99,132,1)',
-                            data: [65, 59, 80, 81, 56, 55, 40],
+                            data: count,
+
                         }
-
                     ],
-
                 }
                 setData(newData)
             })
             .catch((error) => console.error(error))
-    }*/
+    }
     return (
 
         <div className="row">
@@ -79,7 +85,7 @@ function BarGraph() {
                             }],
                             xAxes: [{
                                 scaleLabel: {
-                                    labelString: 'Ages(Years)',
+                                    labelString: 'Ages (Years)',
                                     display: true
                                 }
                             }]
---
+++ web-app/src/components/covid-data/charts/cake.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+
+
+function CakeGraph() {
+    const [data, setData] = useState({
+        labels: [
+            ''
+        ],
+        datasets: [{
+            data: [0],
+            backgroundColor: [
+                '#222831',
+                '#dddddd',
+                '#30475e',
+                '#f05454'
+            ],
+            hoverBackgroundColor: [
+                '#222831',
+                '#dddddd',
+                '#30475e',
+                '#f05454'
+            ]
+        }]
+    })
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+
+            fetchValues()
+
+        }, 1500);
+        return () => clearInterval(interval);
+    });
+
+    const fetchValues = () => {
+        fetch('http://34.67.69.50:7000/infectedtype')
+            .then((response) => response.json())
+            .then((json) => {
+                let labels = []
+                json.forEach(value => {
+                    labels.push(value._id)
+                });
+                let count = []
+                let total = 0
+                json.forEach(value => {
+                    total += value.count
+                });
+                json.forEach(value => {
+                    count.push((value.count / total * 100).toFixed(2))
+                });
+
+                const newData = {
+                    labels: labels,
+                    datasets: [{
+                        data: count,
+                        backgroundColor: [
+                            '#222831',
+                            '#dddddd',
+                            '#f05454',
+                            '#30475e',
+                        ],
+                        hoverBackgroundColor: [
+                            '#222831',
+                            '#dddddd',
+                            '#f05454',
+                            '#30475e',
+                        ]
+                    }]
+                }
+                setData(newData)
+            })
+            .catch((error) => console.error(error))
+    }
+
+    return (
+        <div>
+            <Doughnut data={data} width={500} height={500} />
+        </div>
+    );
+}
+
+export default CakeGraph;
